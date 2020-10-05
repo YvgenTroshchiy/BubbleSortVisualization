21a0884d10cd96d30a1cb66f6b3959c1a946a588
Yvgen Troshchiy <yvgen.troshchiy@gmail.com>
2020-10-05 19:24:44 +0300

    migrate to android X

--- app/src/main/java/com/troshchiy/bubblesortvisualization/ui/bubble_sort_visualization/BubbleSortVisualizationActivity.java
+++ app/src/main/java/com/troshchiy/bubblesortvisualization/ui/bubble_sort_visualization/BubbleSortVisualizationActivity.java
@@ -2,7 +2,7 @@ package com.troshchiy.bubblesortvisualization.ui.bubble_sort_visualization;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.troshchiy.bubblesortvisualization.R;
 
